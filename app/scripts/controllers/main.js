c13a8b5f67ae7472b7806a07013bf0297e09f2af
Takashi Kanemoto <kanemoto.takashi@gmail.com>
2015-06-15 22:30:46 +0900

    enable reply panel

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,8 +1,27 @@
 'use strict';
 
 angular.module('mommodApp')
-    .controller('MainCtrl', ['$scope', 'sampleMarkdownContent', function ($scope, sampleMarkdownContent) {
-        $scope.content = sampleMarkdownContent;
+    .controller('MainCtrl', ['$scope', 'mockTopic', 'mockComments', function ($scope, mockTopic, mockComments) {
+        $scope.myUserName = 'user1';
+
+        $scope.topic = mockTopic;
+        $scope.comments = mockComments;
+
+        $scope.replyings = [];
+        $scope.reply = function (commentId) {
+            $scope.replyings[commentId] = true;
+        };
+        $scope.cancelReply = function (commentId) {
+            $scope.replyings[commentId] = undefined;
+        };
+        $scope.isReplying = function (commentId) {
+            return $scope.replyings[commentId];
+        };
+
+        $scope.isStarred = function (stargazers) {
+            return stargazers.indexOf($scope.myUserName) > -1;
+        };
+
         $scope.isEditing = true;
         $scope.editingContent = '';
     }])
--- app/scripts/services.js
+++ app/scripts/services.js
@@ -1,7 +1,17 @@
 'use strict';
 
 angular.module('mommodApp')
-    .constant('sampleMarkdownContent', '\
+    .constant('mockTopic', {
+        id: 1,
+        title: 'test topic test topic',
+        created_at: new Date(),
+        updated_at: null
+    })
+    .constant('mockComments', [
+        {
+            id: 1,
+            reply_to: null,
+            content: '\
 # test\n\
 \n\
 test test test test test test test test.\n\
@@ -22,6 +32,67 @@ test test test test test test test test test test test.\n\
 echo \'test\';\n\
 ```\n\
 \n\
-http://test.com\n'
-    )
+http://test.com\n',
+            stargazers: [
+                'user1',
+                'user2'
+            ],
+            created_at: new Date(),
+            updated_at: null
+        },
+        {
+            id: 2,
+            reply_to: 1,
+            content: 'test comment',
+            stargazers: [
+                'user1'
+            ],
+            created_at: new Date(),
+            updated_at: null
+        },
+        {
+            id: 3,
+            reply_to: 1,
+            content: 'test comment',
+            stargazers: [
+                'user2'
+            ],
+            created_at: new Date(),
+            updated_at: null
+        },
+        {
+            id: 4,
+            reply_to: 3,
+            content: 'test comment',
+            stargazers: [
+                'user2',
+                'user3'
+            ],
+            created_at: new Date(),
+            updated_at: null
+        },
+        {
+            id: 5,
+            reply_to: 4,
+            content: 'test comment',
+            stargazers: [
+                'user1',
+                'user2'
+            ],
+            created_at: new Date(),
+            updated_at: null
+        },
+        {
+            id: 6,
+            reply_to: 2,
+            content: 'test comment',
+            stargazers: [
+                'user1',
+                'user2',
+                'user3'
+            ],
+            created_at: new Date(),
+            updated_at: null
+        }
+    ])
 ;
