aebddc1c3f314762b5c4d45292b9707b27f2b7a7
Takashi Kanemoto <kanemoto.takashi@gmail.com>
2015-06-17 08:51:47 +0900

    fix scope of comment-view directive

--- app/scripts/directives.js
+++ app/scripts/directives.js
@@ -5,6 +5,10 @@ angular.module('mommodApp')
         return {
             restrict: 'E',
             replace: true,
+            scope: {
+                comment: '=',
+                replyTo: '='
+            },
             templateUrl: 'views/directives/comment-view.html',
             link: function (scope, elem, attr) {
             }
