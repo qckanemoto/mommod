78dfcbd6ecfbbc77fd406f2e11e0d422957fe26c
Takashi Kanemoto <kanemoto.takashi@gmail.com>
2015-06-16 19:54:06 +0900

    fix Gruntfile to enable fontawesome on build

--- Gruntfile.js
+++ Gruntfile.js
@@ -388,6 +388,11 @@ module.exports = function (grunt) {
           cwd: '.',
           src: 'bower_components/bootstrap-sass-official/assets/fonts/bootstrap/*',
           dest: '<%= yeoman.dist %>'
+        }, {
+          expand: true,
+          cwd: 'bower_components/fontawesome/',
+          src: 'fonts/*',
+          dest: '<%= yeoman.dist %>'
         }]
       },
       styles: {
