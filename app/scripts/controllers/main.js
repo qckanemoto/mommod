fefe4b09950b36e21a11c0f84fad33e3956fd9b6
Takashi Kanemoto <kanemoto.takashi@gmail.com>
2015-06-19 12:36:09 +0900

    cosme

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -42,32 +42,33 @@ angular.module('mommodApp')
 
         $scope.signUp = function () {
             var user = new Parse.User();
-            user.set('username', $scope.user.username);
-            user.set('email', $scope.user.email);
-            user.set('password', $scope.user.password);
-
-            user.signUp(null, {
-                success: function (user) {
-                    $scope.$apply(function () {
-                        $rootScope.currentUser = user;
-                        $location.path('list');
-                        $rootScope.alert = {
-                            type: 'success',
-                            message: 'Successfully signed up.',
-                            path: $location.path()
-                        };
-                    });
-                },
-                error: function (user, error) {
-                    $scope.$apply(function () {
-                        $rootScope.alert = {
-                            type: 'danger',
-                            message: '[' + error.code + '] ' + error.message,
-                            path: $location.path()
-                        };
-                    });
-                }
-            });
+            user
+                .set('username', $scope.user.username)
+                .set('email', $scope.user.email)
+                .set('password', $scope.user.password)
+                .signUp(null, {
+                    success: function (user) {
+                        $scope.$apply(function () {
+                            $rootScope.currentUser = user;
+                            $location.path('list');
+                            $rootScope.alert = {
+                                type: 'success',
+                                message: 'Successfully signed up.',
+                                path: $location.path()
+                            };
+                        });
+                    },
+                    error: function (user, error) {
+                        $scope.$apply(function () {
+                            $rootScope.alert = {
+                                type: 'danger',
+                                message: '[' + error.code + '] ' + error.message,
+                                path: $location.path()
+                            };
+                        });
+                    }
+                })
+            ;
         };
     }])
 ;
