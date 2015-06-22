2e13c11f4f5ebabbe0fbf57a30d668dd68d1071e
Takashi Kanemoto <kanemoto.takashi@gmail.com>
2015-06-22 16:06:59 +0900

    update topic page

--- app/scripts/controllers/topic.js
+++ app/scripts/controllers/topic.js
@@ -1,36 +1,96 @@
 'use strict';
 
 angular.module('mommodApp')
-    .controller('TopicCtrl', ['$scope', '$routeParams', 'mockTopic', 'mockComments', 'assertSignedIn', function ($scope, $routeParams, mockTopic, mockComments, assertSignedIn) {
-        assertSignedIn();
+    .controller('TopicCtrl', [
+        '$scope', '$rootScope', '$location', '$anchorScroll', '$routeParams', '$timeout', 'assertSignedIn', 'cachedParseQuery',
+        function ($scope, $rootScope, $location, $anchorScroll, $routeParams, $timeout, assertSignedIn, cachedParseQuery) {
 
-        $scope.myUserName = 'user1';
+            assertSignedIn();
 
-        $scope.topicId = $routeParams.topicId;
+            $scope.topic = null;
+            $scope.comments = [];
+            $scope.replyTo = null;
+            $scope.commentContent = '';
 
-        $scope.topic = mockTopic;
-        $scope.comments = mockComments;
+            var query = {
+                topic: new Parse.Query('Topic'),
+                comment: new Parse.Query('Comment')
+            };
 
-        $scope.replyTo = null;
-        $scope.commentContent = '';
+            // get topic and comments.
+            cachedParseQuery(query.topic.equalTo('objectId', $routeParams.topicId).include('user'), 'first')
+                .then(function (topic) {
+                    $scope.topic = topic;
+                    return Parse.Promise.as(topic);
+                })
+                .then(function (topic) {
+                    return cachedParseQuery(query.comment.equalTo('topic', topic).include('user').ascending('createdAt'), 'find');
+                })
+                .then(function (comments) {
+                    $scope.comments = comments;
+                    return Parse.Promise.as();
+                })
+                .done(function () {
+                    $timeout();
+                    $timeout($anchorScroll);  // anchorScroll after comments are rendered.
+                })
+                .fail(function (error) {
+                })
+            ;
 
-        $scope.submission = {};
-        $scope.$watch('submission', function () {
-            if ($scope.submission.submit) {
-            }
-        });
+            $scope.createComment = function () {
+                var acl = new Parse.ACL();
+                acl.setPublicReadAccess(true);
+                acl.setPublicWriteAccess(false);
+                acl.setReadAccess($rootScope.currentUser.id, true);
+                acl.setWriteAccess($rootScope.currentUser.id, true);
 
-        $scope.isStarred = function (comment) {
-            return comment.stargazers.indexOf($scope.myUserName) > -1;
-        };
-        $scope.toggleStar = function (comment) {
-            var idx = comment.stargazers.indexOf($scope.myUserName);
-            if (idx == -1) {
-                comment.stargazers.push($scope.myUserName);
-            } else {
-                delete comment.stargazers[idx];
-                comment.stargazers = _.compact(comment.stargazers);
-            }
-        };
-    }])
+                var topic = new Parse.Object('Comment');
+                topic
+                    .set('content', $scope.commentContent)
+                    .set('user', $rootScope.currentUser)
+                    .set('topic', $scope.topic)
+                    .set('replyTo', $scope.replyTo)
+                    .setACL(acl)
+                    .save()
+                    .done(function (comment) {
+                        $scope.$apply(function () {
+                            $scope.comments.push(comment);
+                            $scope.commentContent = '';
+                            $scope.replyTo = null;
+                        });
+                    })
+                    .fail(function (error) {
+                        $scope.$apply(function () {
+                            $rootScope.alert = {
+                                type: 'danger',
+                                message: '[' + error.code + '] ' + error.message,
+                                path: $location.path()
+                            };
+                        });
+                    })
+                ;
+            };
+
+
+            $scope.submission = {};
+            $scope.$watch('submission', function () {
+                if ($scope.submission.submit) {
+                }
+            });
+
+            $scope.isStarred = function (comment) {
+                return comment.stargazers.indexOf($scope.myUserName) > -1;
+            };
+            $scope.toggleStar = function (comment) {
+                var idx = comment.stargazers.indexOf($scope.myUserName);
+                if (idx == -1) {
+                    comment.stargazers.push($scope.myUserName);
+                } else {
+                    delete comment.stargazers[idx];
+                    comment.stargazers = _.compact(comment.stargazers);
+                }
+            };
+        }
+    ])
 ;
--- app/scripts/services.js
+++ app/scripts/services.js
@@ -12,7 +12,7 @@ angular.module('mommodApp')
         return function (parseQuery, method) {
             var parseCache = $cacheFactory.get('parseQuery') || $cacheFactory('parseQuery');
 
-            var key = JSON.stringify(parseQuery);
+            var key = JSON.stringify(parseQuery) + '#' + method;
 
             if (parseCache.get(key) != undefined) {
                 return Parse.Promise.as(parseCache.get(key));
@@ -31,106 +31,6 @@ angular.module('mommodApp')
             return promise;
         };
     }])
-    .constant('mockTopic', {
-        id: 1,
-        title: 'test topic test topic',
-        content: '\
-# test\n\
-\n\
-test test test test test test test test.\n\
-test test test test test test test test test test test.\n\
-\n\
-## test\n\
-\n\
-test test test test test test test test.\n\
-\n\
-* test\n\
-  * test\n\
-* test\n\
-\n\
-test test test test test test `test test` test test test.\n\
-\n\
-### test\n\
-\n\
-1. test\n\
-  * test\n\
-  * test\n\
-1. test\n\
-\n\
-![](https://avatars2.githubusercontent.com/u/4360663)\n\
-\n\
-```\n\
-<?php\n\
-// test\n\
-echo \'test\';\n\
-```\n\
-\n\
-| test | test |\n\
-| --- | --- |\n\
-| aaa | aaa |\n\
-| bbb | bbb |\n\
-\n\
-> test\n\
-> > test\n\
-> test\n\
-\n\
-http://test.com\n\
-\n\
-emoji :+1: emoji',
-        created_at: new Date(),
-        updated_at: null
-    })
-    .constant('mockComments', [
-        {
-            id: 2,
-            content: 'test comment',
-            stargazers: [
-                'user1'
-            ],
-            created_at: new Date(),
-            updated_at: null
-        },
-        {
-            id: 3,
-            content: 'test comment',
-            stargazers: [
-                'user2'
-            ],
-            created_at: new Date(),
-            updated_at: null
-        },
-        {
-            id: 4,
-            content: 'test comment',
-            stargazers: [
-                'user2',
-                'user3'
-            ],
-            created_at: new Date(),
-            updated_at: null
-        },
-        {
-            id: 5,
-            content: 'test comment',
-            stargazers: [
-                'user1',
-                'user2'
-            ],
-            created_at: new Date(),
-            updated_at: null
-        },
-        {
-            id: 6,
-            content: 'test comment',
-            stargazers: [
-                'user1',
-                'user2',
-                'user3'
-            ],
-            created_at: new Date(),
-            updated_at: null
-        }
-    ])
     .constant('mockThread', [
         {
             id: 2,
